a3883ea076fecfa31c7c5471e82233c7adb55403
Bartok Peter Laszlo (Gyula) <78101657+barpet007@users.noreply.github.com>
2025-11-02 16:46:44 +0100

    GREEN: Implement Rover class, constructor, and getters

---
+++ src/main/java/hu/uni/miskolc/Rover.java
@@ -0,0 +1,28 @@
+package hu.uni.miskolc;
+
+public class Rover {
+
+    private int x;
+    private int y;
+    private char direction;
+
+    // Konstruktor, ami beállítja a kezdőértékeket
+    public Rover(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    // Getterek, amiket a teszt hív
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public char getDirection() {
+        return this.direction;
+    }
+}
